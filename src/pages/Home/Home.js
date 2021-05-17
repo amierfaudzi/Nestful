04e1be6eaeb8f0b52510a32c5cf81dc11104acd1
amierfaudzi <amier.faudzi@gmail.com>
2021-05-16 23:15:45 -0400

    add styles to the Feature section in the Landing Page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import './Home.scss';
+import { ReactComponent as StartUp } from '../../assets/Startup.svg';
+import { ReactComponent as Valuation } from '../../assets/Valuations.svg';
+import { ReactComponent as Growth } from '../../assets/Growth.svg';
+import { ReactComponent as TextLine } from '../../assets/textLine.svg';
+
 
 export default function Home() {
     return (
@@ -15,33 +20,56 @@ export default function Home() {
                     <p>Scroll down to begin</p>
                 </div>
             </section>
-            <section className="features">
-                <div>
-                    <h2>Nestful is built to support you during the entire retirement journey</h2>
-                    <p>
-                        Designed to help near-retires and retirees accomplish their goals and live their retirement years with a sense of purpose.
-                    </p>
+            <section className="section__features">
+                <div className="section__features--header">
+                    <div className="section__features--header-title">
+                        <h2>Nestful is built to support you during the entire retirement journey</h2>
+                    </div>
+                    <div className="section__features--header-subtitle">
+                        <p>
+                            Designed to help near-retires and retirees accomplish their goals and live their retirement years with a sense of purpose.
+                        </p>
+                    </div>
                 </div>
-                <div>
-                    <p>Features</p>
-                    <div>
-                        <div>
-                            Retirement Check-in
+                <div className="container--center">
+                    <div className="introduction-feature">
+                        <TextLine/><p className="introduction-feature__title">Features</p><TextLine/>
+                    </div>
+                    
+                    <div className="container--feature">
+                        <div className="feature">
+                            <div className="feature__iconBg">
+                                <Valuation className="feature__icon"/>
+                            </div>
+                            <h4>Retirement Check-in</h4>
+                            <div className="feature__description">
                             <p>
                                 View the current future financial scenarios for your retirement years.
                             </p>
+                            </div>
                         </div>
-                        <div>
-                            Retirement Goals
+                        <div className="feature">
+                            <div className="feature__iconBg">
+                                <StartUp className="feature__icon"/>
+                            </div>
+                            <h4>Retirement Goals</h4>
+                            <div className="feature__description">
                             <p>
                                 Set financial and lifestyle goals for retirement and track your progress.
                             </p>
+                            </div>
                         </div>
-                        <div>
-                            Retirement Finances
-                            <p>
-                                Create budgets, track spending, and save for your retirement travels.
-                            </p>
+                        <div className="feature">
+                            <div className="feature__iconBg">
+                            <Growth className="feature__icon"/>
+                            </div>
+                            <h4>Retirement Finances</h4>
+                            <div className="feature__description">
+                                <p>
+                                    Create budgets, track spending, and save for your retirement travels.
+                                </p>
+                            </div>
+
                         </div>
                     </div>
                 </div>
